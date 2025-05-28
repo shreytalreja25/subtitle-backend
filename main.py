a577bb57b7c9e98738ec81d239cd62641dabdcb3
shreytalreja25 <shreytalreja25@gmail.com>
2025-05-28 18:36:05 +1000

    cors update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # replace with frontend URL in prod
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
